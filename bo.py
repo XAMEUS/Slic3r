f131479076c28b99c68b10d4340968766986ea3c
Maxime <maxime.gourgoulhon@gmail.com>
2017-04-18 13:13:08 +0200

    Simple init + loop Création des évènements ordonnés de début et de fin de segments, stoqués dans une SortedList. Parcours progressifs des évènements avec des affichages pour effectuer l'algorithme pas à pas.

--- bo.py
+++ bo.py
@@ -10,13 +10,42 @@ for each file:
 import sys
 from geo.segment import load_segments
 from geo.tycat import tycat
+from sortedcontainers import SortedList
 
 def test(filename):
     """
     run bentley ottmann
     """
     adjuster, segments = load_segments(filename)
-    tycat(segments)
+    #segments = sorted(segments, key=lambda x: min(x.endpoints[0].coordinates[1],
+    #                                              x.endpoints[0].coordinates[1]))
+    events = SortedList()
+    for s in segments:
+        events.add((min(s.endpoints), "in", s))
+        events.add((max(s.endpoints), "out", s))
+    SL = SortedList()
+    IL = []
+    print("Events (init):", events)
+    print("\n========\n  LOOP  \n========\n\n   ")
+    while True:
+        try:
+            p, t, s = events.pop(0)
+
+            print("Current:", p, t, s)
+            print("Events:", events)
+            print("SL:", len(SL), SL)
+
+            tycat(segments, IL, p)
+            input("Press [ENTER] to continue...\n")
+
+        except IndexError:
+            break
+
+    print("\n\n=========\n THE END\n=========")
+    print("Events:", events)
+    print("SL:", SL)
+    print("IL:", IL)
+    tycat(segments, IL)
     #TODO: merci de completer et de decommenter les lignes suivantes
     #results = lancer bentley ottmann sur les segments et l'ajusteur
     #...
