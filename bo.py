fe897d5f051577cae9976b518dbbc93fd4b5ef2d
Maxime <maxime.gourgoulhon@gmail.com>
2017-04-27 15:53:16 +0200

    heapq

--- bo.py
+++ bo.py
@@ -10,6 +10,7 @@ for each file:
 import sys
 from geo.segment import load_segments
 from geo.tycat import tycat
+from heapq import heappush, heappop
 
 def test(filename):
     """
@@ -17,6 +18,12 @@ def test(filename):
     """
     adjuster, segments = load_segments(filename)
     tycat(segments)
+
+    events = []
+    for segment in segments:
+        heappush(events, (min(segment.endpoints), "in", segment))
+        heappush(events, (max(segment.endpoints), "out", segment))
+
     #TODO: merci de completer et de decommenter les lignes suivantes
     #results = lancer bentley ottmann sur les segments et l'ajusteur
     #...
