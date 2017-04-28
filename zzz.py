318cbb334c24ed0ba8aaa55e3a64794c77fbc1ec
Maxime <maxime.gourgoulhon@gmail.com>
2017-04-28 17:58:21 +0200

    zzz

---
+++ zzz.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+"""
+this tests bentley ottmann on given .bo files.
+for each file:
+    - we display segments
+    - run bentley ottmann
+    - display results
+    - print some statistics
+"""
+import sys
+from heapq import heappush, heappop
+from sortedcontainers import SortedList
+from geo.tycat import tycat
+from geo.segment import load_segments, load_segments_stdin, Segment
+
+DEBUG = True
+ENTER = True
+
+def load_events(segments_origin, events, dict_seg):
+    """
+    Load all events
+    """
+    for segment in segments_origin: #On ajoute les événements adéquats
+        pt_min, pt_max = min(segment.endpoints), max(segment.endpoints)
+        heappush(events, pt_min)
+        heappush(events, pt_max)
+        if pt_min in dict_seg:
+            dict_seg[pt_min][0].append(segment)
+        else:
+            dict_seg[pt_min] = [[segment], []]
+        if pt_max in dict_seg:
+            dict_seg[pt_max][1].append(segment)
+        else:
+            dict_seg[pt_max] = [[], [segment]]
+
+def load_file(filename):
+    """
+    Load file
+    """
+    if filename is not None:
+        adjuster, segments_origin = load_segments(filename)
+    else:
+        adjuster, segments_origin = load_segments_stdin()
+    tycat(segments_origin)
+    return adjuster, segments_origin
+
+def test(filename):
+    """
+    run bentley ottmann
+    """
+    events = [] #Tas des événements: (point, type_d_evenement)
+    dict_seg = {} #Dictionnaire contenant les segments au point (point, type_d_evenement)
+    sweep = SortedList() #(Sorted)List des segments en vie
+    results = [] #Les points finaux
+    nb_coupes = 0 #Si un point d'intersection apparait dans plusieurs segments,
+                    #il compte plusieurs fois
+
+    adjuster, segments_origin = load_file(filename)
+    Segment.adjuster = adjuster
+    load_events(segments_origin, events, dict_seg)
+
+    while events: #Traitement des événements
+        current = heappop(events)
+
+        segments = dict_seg[current]
+
+        Segment.point =  current
+
+        if DEBUG:
+            print("Current:", current, segments)
+            print("Events:", events)
+            print("SL:", len(sweep), sweep)
+            tycat(segments_origin, results, current, sweep, segments[0], segments[1])
+
+        if segments[1]:
+            for segment in segments[1]:
+                i = sweep.index(segment)
+                sweep.remove(segment)
+
+        Segment.point = current
+
+        if segments[0]:
+            for segment in segments[0]:
+                sweep.add(segment)
+                i = sweep.index(segment)
+                left = i-1
+                if left >= 0:
+                    left = sweep[left]
+                    intrsctn = segment.intersection_with(left)
+                    if intrsctn and \
+                        intrsctn.coordinates[1] <= current.coordinates[1] and\
+                        intrsctn.coordinates[0] != current.coordinates[0]:
+                        intrsctn = adjuster.hash_point(intrsctn)
+                        Segment.point = intrsctn
+                        sweep.remove(left)
+                        if segment in sweep:
+                            sweep.remove(segment)
+                        results.append(intrsctn)
+                        sweep.add(left)
+                        sweep.add(segment)
+
+                right = i + 1
+                if right < len(sweep):
+                    right = sweep[right]
+                    intrsctn = segment.intersection_with(right)
+                    if intrsctn and \
+                        intrsctn.coordinates[1] <= current.coordinates[1] and\
+                        intrsctn.coordinates[0] != current.coordinates[0]:
+                        intrsctn = adjuster.hash_point(intrsctn)
+                        Segment.point = intrsctn
+                        sweep.remove(right)
+                        if segment in sweep:
+                            sweep.remove(segment)
+                        results.append(intrsctn)
+                        sweep.add(right)
+                        sweep.add(segment)
+
+        if DEBUG:
+            print("Current:", current, segments)
+            print("Events:", events)
+            print("SL:", len(sweep), sweep)
+            print(results)
+            tycat(segments_origin, results, current, sweep)
+        if ENTER:
+            input("Press [ENTER] to continue...\n")
+    tycat(segments_origin, results)
+    if ENTER:
+        input("Press [ENTER] to continue...\n")
+    print("le nombre d'intersections (= le nombre de points differents) est", len(results))
+    print("le nombre de coupes dans les segments est", nb_coupes)
+
+def main():
+    """
+    launch test on each file.
+    """
+    if len(sys.argv) == 1:
+        test(None)
+    for filename in sys.argv[1:]:
+        test(filename)
+
+main()
