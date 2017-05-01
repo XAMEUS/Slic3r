96f3828425cc0b8610541f1637d24a4a1ca94982
Maxime <maxime.gourgoulhon@gmail.com>
2017-05-01 13:10:30 +0200

    remove debugs

--- bo.py
+++ bo.py
@@ -12,12 +12,6 @@ from heapq import heappush, heappop
 from sortedcontainers import SortedList
 from geo.tycat import tycat
 from geo.segment import load_segments, load_segments_stdin, Segment, key
-from debug import debug_sweep, debug_print, debug_pause
-
-DEBUG = False
-PAUSE = False
-MORE = False
-STOP = []
 
 def load_events(segments_origin, events, dict_seg, results):
     """
@@ -84,93 +78,45 @@ def test(filename):
     Segment.adjuster = adjuster
     load_events(segments_origin, events, dict_seg, results)
 
-    count = 0
-
     while events: #Traitement des événements
-        count += 1
-        # debug_print(("ITER:", count))
         current = heappop(events) #On récupère le point à traiter
         segments = dict_seg[current] #On récupèrer ses segments associés (in, out)
-        if DEBUG or count in STOP:
-            # print("Events:", events)
-            # print("SL:", len(sweep), sweep)
-            tycat(segments_origin, results, current, sweep, segments[0], segments[1])
-            print("###############")
-            print("Current:", current, segments)
-            print("###############")
 
         if segments[1]: #On traite les out
             while segments[1]:
                 segment = segments[1].pop()
 
-
-                if MORE or count in STOP:
-                    tycat(segments_origin, results, current, sweep, segment)
-                debug_print(("DO : OUT", segment), DEBUG)
                 i = sweep.bisect_left(segment)
-
-                # DEBUG
-                debug_print(("on cherche", key(segment, current), segment), DEBUG)
-                debug_print(("default i =", i), DEBUG)
-                debug_sweep(sweep, current, DEBUG, PAUSE)
-                # END DEBUG
 
                 if sweep[i] != segment and i < len(sweep)-1:
                     for i, tmp in enumerate(sweep):
                         if tmp == segment:
-                            debug_print(("real i =", i), DEBUG)
-                            debug_pause(pause=PAUSE)
                             break
                 left = i-1-(i > 1 and key(sweep[i-1], current) == key(segment, current))
                 right = i+1+(i < len(sweep)-1 and key(sweep[i+1], current) == key(segment, current))
 
-                debug_print(("default i =", i), DEBUG)
                 if left >= 0 and right < len(sweep):
                     test_intersect(results, dict_seg, events, adjuster, current,
                                    [sweep[left], sweep[right]])
 
-                # DEBUG
-                debug_print(("del i =", i), DEBUG)
-                debug_sweep(sweep, current, DEBUG, PAUSE)
-                # END DEBUG
                 del sweep[i]
-                # DEBUG
-                debug_sweep(sweep, current, DEBUG, PAUSE)
-                # END DEBUG
 
-        if MORE or count in STOP:
-            print("###### IN", current)
         Segment.point = current #On actualise le point: pt de référence
 
         if segments[0]: #On traite les in
             while segments[0]:
-                #print(segments[0])
                 segment = segments[0].pop()
-                #print(segments[0])
-                if MORE or count in STOP:
-                    tycat(segments_origin, results, current, sweep, segment)
-                debug_print(("DO : IN", segment), DEBUG)
-                sweep.add(segment)
-                debug_sweep(sweep, current, DEBUG, PAUSE)
-                debug_print(("add", segment), DEBUG)
-                i = sweep.bisect_left(segment)
-                # DEBUG
-                debug_print(("on cherche", key(segment, current), segment), DEBUG)
-                debug_print(("default i =", i), DEBUG)
-                debug_sweep(sweep, current, DEBUG, PAUSE)
-                # END DEBUG
 
+                sweep.add(segment)
+                i = sweep.bisect_left(segment)
 
                 if sweep[i] != segment and i < len(sweep)-1:
                     for i, tmp in enumerate(sweep):
                         if tmp == segment:
-                            debug_print(("real i =", i), DEBUG)
-                            debug_pause(pause=PAUSE)
                             break
                 left = i-1-(i > 1 and key(sweep[i-1], current) == key(segment, current))
                 right = i+1+(i < len(sweep)-1 and key(sweep[i+1], current) == key(segment, current))
 
-                debug_print(("left:", left, ", right:", right), DEBUG)
                 #On traite à gauche
                 if left >= 0:
                     test_intersect(results, dict_seg, events, adjuster, current,
@@ -182,17 +128,7 @@ def test(filename):
                                    [segment, sweep[right]])
                 #print(segments[0])
 
-        if DEBUG or count in STOP:
-            # print("Events:", events)
-            # print("SL:", len(sweep), sweep)
-            print("############### END WHILE")
-            print("Current:", current, segments)
-            tycat(segments_origin, results, current, sweep)
-        if PAUSE:
-            input("Press [ENTER] to continue...\n")
     tycat(segments_origin, results)
-    if PAUSE:
-        input("Press [ENTER] to continue...\n")
     print("le nombre d'intersections (= le nombre de points differents) est", len(set(results)))
     print("le nombre de coupes dans les segments est", len(results))
 
